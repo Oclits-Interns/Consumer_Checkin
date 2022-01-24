e324094ec5c61e067f96067be54cadb42c70eb30
Ali Mohsin Naqvi <ali.m.naqvi110@gmail.com>
2022-01-24 15:37:19 +0500

    Home screen layout implemented

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:consumer_checkin/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -12,9 +13,24 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Home"),
-        centerTitle: true,
+        backgroundColor: kMaroon,
+        leading: Icon(Icons.search, color: Colors.black,),
       ),
+      body: ListView.builder(
+                itemCount: 15,
+                  itemBuilder: (context, index) {
+                    return index == 0 ? const ListTile(
+                      contentPadding: EdgeInsets.all(8.0),
+                      title: Text("Open Map"),
+                      leading: Icon(Icons.location_on, color: Colors.red, size: 48),
+                      trailing: Icon(Icons.arrow_forward_ios_rounded, color: Colors.black, size: 28),
+                    ) : const ListTile(
+                      contentPadding: EdgeInsets.all(8.0),
+                      title: Text("Consumer Id"),
+                      leading: Icon(Icons.location_on, color: Colors.red, size: 48,),
+                      trailing: Icon(Icons.arrow_forward_ios_rounded, color: Colors.black, size: 28),
+                    );
+                  }),
     );
   }
 }
